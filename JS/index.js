3fc59da0ddcb35eaa7a8249ba89a4b2c3eac6711
Robert Driskell <driskell.ba@gmail.com>
2019-04-16 11:26:12 -0500

    Improved scrolling feature so it will persist across any number of sections

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,20 @@
 //Javascript
 
+
+//Scrolling Sections
 $("html,body").animate({scrollTop: 0}, 50, 'swing');
 
-const sections = document.querySelectorAll('section');
-console.log(sections);
-console.log(sections.length);
+const sections = document.querySelectorAll('.scroll-to');
+// console.log(sections);
+// console.log(sections.length);
 var dist = 0;
+
+console.log(sections[dist]);
+
+for (let i = 0; i < sections.length; i++) {
+    sections[i].id = `sectionElement${i}`;
+    console.log(sections[i]);
+};
 
 window.addEventListener('wheel', event => {
     event.preventDefault();
@@ -13,17 +22,18 @@ window.addEventListener('wheel', event => {
 
     // Sets dist to next section
     if (event.deltaY > 0) {
-        if ( dist < window.innerHeight*(sections.length-1) ){
-            dist += window.innerHeight;
+        if ( dist < sections.length-1) {
+            dist += 1;
             // Animates a scroll to the current set distance
-            $("html,body").animate({scrollTop: dist}, 500, 'swing');
+            $("html,body").animate({scrollTop: $(`#sectionElement${dist}`).offset().top }, 500, 'swing');
+            // console.log(`#sectionElement${dist}`);
         }
     }
 
     if (event.deltaY < 0) {
         if ( dist > 0 ){
-            dist -= window.innerHeight;
-            $("html,body").animate({scrollTop: dist}, 500, 'swing');
+            dist -= 1;
+            $("html,body").animate({scrollTop: $(`#sectionElement${dist}`).offset().top }, 500, 'swing');
         }
     }
 });
@@ -34,18 +44,19 @@ window.addEventListener('keydown', event => {
     // Sets dist to next section
     if (event.key == 'ArrowDown' || event.key == 'PageDown') {
         event.preventDefault();
-        if ( dist < window.innerHeight*(sections.length-1) ){
-            dist += window.innerHeight;
+        if ( dist < sections.length-1) {
+            dist += 1;
             // Animates a scroll to the current set distance
-            $("html,body").animate({scrollTop: dist}, 500, 'swing');
+            $("html,body").animate({scrollTop: $(`#sectionElement${dist}`).offset().top }, 500, 'swing');
         }
     }
 
     if (event.key == 'ArrowUp' || event.key == 'PageUp') {
         event.preventDefault();
         if ( dist > 0 ){
-            dist -= window.innerHeight;
-            $("html,body").animate({scrollTop: dist}, 500, 'swing');
+            dist -= 1;
+            $("html,body").animate({scrollTop: $(`#sectionElement${dist}`).offset().top }, 500, 'swing');
         }
     }
 });
+
