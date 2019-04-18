5528bbbba8dae7191d34d261401095870ac21289
Robert Driskell <driskell.ba@gmail.com>
2019-04-18 16:56:20 -0500

    Scrolling bugs fixed, code's a little hacky, but deadlines are deadlines. Me happy.

--- JS/scroll.js
+++ JS/scroll.js
@@ -10,6 +10,8 @@
 
 // $(document).on('scroll', function() {})
 
+let currentlyAnimating = 0;
+
 class Section {
     constructor(sectionElement){
         this.sectionElement = sectionElement;
@@ -28,76 +30,89 @@ class Section {
 
         this.fadeIn = this.fadeIn.bind(this);
 
-        this.sectionElement.addEventListener('wheel', event => {
-            if (this.checkPos(this.sectionElement)) {
-                this.changeClassMarker();
-                this.changeArrows();
-                this.fadeIn();
-            }
+        this.callMethods = this.callMethods.bind(this);
+
+        this.callMethods();
+
+        window.addEventListener('wheel', event => {
+            this.callMethods();
+        })
+
+        window.addEventListener('fake-event', event => {
+            this.callMethods();
         })
 
     }
 
     checkPos(element){
-        console.log('yo');
-        if($(window).scrollTop()>=$(element).position().top &&
-        $(window).scrollTop()< ($(element).position().top + $(window).height()) ){
+        if(($(window).scrollTop() + ($(window).height()/2))>=$(element).position().top &&
+        ($(window).scrollTop() + ($(window).height()/2))< ($(element).position().top + $(window).height()) ){
             return true;
+        }
+        else {
+            return false;
         }
     }
 
     changeArrows(){
-        if (this.sectionLight = 'light') {
-            if (this.sectionPos == 'bottom') {
-                $('.arrow-down').animate({
-                    color: jQuery.Color({alpha: 0})
-                }, 100, 'swing')
-                $('.arrow-up').animate({
-                    color: jQuery.Color("#06061C")
-                }, 500, 'swing')
-            }
-            else if (this.sectionPos == 'top') {
-                $('.arrow-up').animate({
-                    color: jQuery.Color({alpha: 0})
-                }, 100, 'swing')
-                $('.arrow-down').animate({
-                    color: jQuery.Color("#06061C")
-                }, 500, 'swing')
-            }
-            else {
-                $('.arrow-up').animate({
-                    color: jQuery.Color("#06061C")
-                }, 500, 'swing')
-                $('.arrow-down').animate({
-                    color: jQuery.Color("#06061C")
-                }, 500, 'swing')
-            }
-        }
-        else {
-            if (this.sectionPos == 'bottom') {
-                $('.arrow-down').animate({
-                    color: jQuery.Color({alpha: 0})
-                }, 100, 'swing')
-                $('.arrow-up').animate({
-                    color: jQuery.Color("#E3EBED")
-                }, 500, 'swing')
-            }
-            else if (this.sectionPos == 'top') {
-                $('.arrow-up').animate({
-                    color: jQuery.Color({alpha: 0})
-                }, 100, 'swing')
-                $('.arrow-down').animate({
-                    color: jQuery.Color("#E3EBED")
-                }, 500, 'swing')
+        // console.log(currentlyAnimating);
+        if (currentlyAnimating == 0) {
+            currentlyAnimating = 1;
+            if (this.sectionLight == 'light') {
+                if (this.sectionPos == 'bottom') {
+                    $('.arrow-down').animate({
+                        color: jQuery.Color({alpha: 0})
+                    }, 100, 'swing')
+                    $('.arrow-up').animate({
+                        color: jQuery.Color("#06061C")
+                    }, 500, 'swing')
+                }
+                else if (this.sectionPos == 'top') {
+                    $('.arrow-up').animate({
+                        color: jQuery.Color({alpha: 0})
+                    }, 100, 'swing')
+                    $('.arrow-down').animate({
+                        color: jQuery.Color("#06061C")
+                    }, 500, 'swing')
+                }
+                else {
+                    $('.arrow-up').animate({
+                        color: jQuery.Color("#06061C")
+                    }, 500, 'swing')
+                    $('.arrow-down').animate({
+                        color: jQuery.Color("#06061C")
+                    }, 500, 'swing')
+                }
             }
             else {
-                $('.arrow-up').animate({
-                    color: jQuery.Color("#E3EBED")
-                }, 500, 'swing')
-                $('.arrow-down').animate({
-                    color: jQuery.Color("#E3EBED")
-                }, 500, 'swing')
+                if (this.sectionPos == 'bottom') {
+                    $('.arrow-down').animate({
+                        color: jQuery.Color({alpha: 0})
+                    }, 100, 'swing')
+                    $('.arrow-up').animate({
+                        color: jQuery.Color("#E3EBED")
+                    }, 500, 'swing')
+                }
+                else if (this.sectionPos == 'top') {
+                    $('.arrow-up').animate({
+                        color: jQuery.Color({alpha: 0})
+                    }, 100, 'swing')
+                    $('.arrow-down').animate({
+                        color: jQuery.Color("#E3EBED")
+                    }, 500, 'swing')
+                }
+                else {
+                    $('.arrow-up').animate({
+                        color: jQuery.Color("#E3EBED")
+                    }, 500, 'swing')
+                    $('.arrow-down').animate({
+                        color: jQuery.Color("#E3EBED")
+                    }, 500, 'swing')
+                }
             }
+            setTimeout(function() {
+                currentlyAnimating = 0;
+            }, 300);
         }
     }
 
@@ -118,7 +133,7 @@ class Section {
                 if (i >0) {
                     sections[i-1].classList.add('previous-section');
                 }
-                if (i < sections.length) {
+                if (i < sections.length-1) {
                     sections[i+1].classList.add('next-section');
                 }
             }
@@ -127,19 +142,19 @@ class Section {
     }
 
     fadeIn() {
-        if(this.sectionLight == 'light') {
-            this.fadableElements.forEach(element => {
-                $(element).animate({
-                    color: jQuery.Color("#06061C")
-                }, 500, 'swing')
-            })
-        }
-        else {
-            this.fadableElements.forEach(element => {
-                $(element).animate({
-                    color: jQuery.Color("#E3EBED")
-                }, 500, 'swing')
-            })
+        // console.log(this.fadableElements);
+        this.fadableElements.forEach(element => {
+            $(element).delay(100).fadeIn('slow')
+        });
+    }
+
+    callMethods() {
+        if (this.checkPos(this.sectionElement)) {
+            // console.log(this.checkPos(this.sectionElement));
+            this.changeClassMarker();
+            this.changeArrows();
+            this.fadeIn();
+            // console.log(this.sectionLight + ' ' + this.sectionPos)
         }
     }
 }
@@ -147,16 +162,28 @@ class Section {
 const upArrow = document.querySelector('.arrow-up');
 const downArrow = document.querySelector('.arrow-down');
 
+var fakeEvent = new CustomEvent("fake-event", { "solution": "Bad" });
+
 downArrow.addEventListener('click', event => {
     $("html,body").animate({scrollTop: $(`.next-section`).offset().top }, 500, 'swing');
+    setTimeout(function () {
+        window.dispatchEvent(fakeEvent);
+    },500)
 });
 
 upArrow.addEventListener('click', event => {
     $("html,body").animate({scrollTop: $(`.previous-section`).offset().top }, 500, 'swing');
+    setTimeout(function () {
+        window.dispatchEvent(fakeEvent);
+    },500)
 });
 
 const sections = document.querySelectorAll('.scroll-to');
 
 sections.forEach(element => {
     new Section(element);
+})
+
+window.addEventListener('wheel', element => {
+    console.log('yo');
 })
